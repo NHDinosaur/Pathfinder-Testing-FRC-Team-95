2e3aa1da71b73c94d63c8b6e8bf42014388dc3f0
PJG3000\Philip <philip@sugargroup.net>
2020-01-25 15:37:56 -0500

    Got a working autonomous mode for the robot.

--- PathfinderTestBotTeam95/src/main/java/frc/robot/Robot.java
+++ PathfinderTestBotTeam95/src/main/java/frc/robot/Robot.java
@@ -7,29 +7,103 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.PWMVictorSPX;
+//import edu.wpi.first.wpilibj.Controller;
+//import edu.wpi.first.wpilibj.Joystick;
+//import edu.wpi.first.wpilibj.PWMVictorSPX;
 import edu.wpi.first.wpilibj.TimedRobot;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.GenericHID.Hand;
+
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.IMotorControllerEnhanced;
+//import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
  * it contains the code necessary to operate a robot with tank drive.
  */
 public class Robot extends TimedRobot {
-  private DifferentialDrive m_myRobot;
-  private Joystick m_leftStick;
-  private Joystick m_rightStick;
+  //private DifferentialDrive m_myRobot;
+  // private static final int LeftStickY = 1;
+  // private static final int RightStickY = 5;
+
+  // private Joystick LeftStick;
+  // private Joystick RightStick;
+
+  // private Joystick XBC;
+
+  private XboxController Controller = new XboxController(1);;
+
+  private IMotorControllerEnhanced Left, Right;
+
+  private int leftCanNum = 10;
+  private int rightCanNum = 22;
+
+  private double Time = 0;
+
+  // private double timeAtStop = 10;
 
   @Override
   public void robotInit() {
-    m_myRobot = new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
-    m_leftStick = new Joystick(0);
-    m_rightStick = new Joystick(1);
+    Left = new TalonSRX(leftCanNum); //new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    Right = new TalonSRX(rightCanNum);
+    // LeftStick = new Joystick(1);
+    // RightStick = new Joystick(5);
   }
 
   @Override
   public void teleopPeriodic() {
-    m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
+    // m_myRobot.tankDrive(LeftStick.getY(), RightStick.getY());
+    Left.set(ControlMode.PercentOutput, getLeftY());
+    Right.set(ControlMode.PercentOutput, getRightY());
+  }
+
+  @Override
+  public void autonomousInit() {
+
+  }
+
+  @Override
+  public void autonomousPeriodic() {
+    // System.out.println("We are in auto periodic");
+
+    // Left.set(ControlMode.PercentOutput, 20);
+    // Right.set(ControlMode.Position, 1);
+    Time++;
+    System.out.println("Time = " + Time);
+
+    if(Time <= 50)
+    {
+      Left.set(ControlMode.PercentOutput, 1);
+    }
+    else if(Time >= 50 && Time <= 100)
+    {
+      Left.set(ControlMode.PercentOutput, 0.5);
+    }
+    else
+    {
+      Left.set(ControlMode.Disabled, 0);
+    }
+  }
+
+  public double getLeftY() {
+    return Controller.getY(Hand.kLeft);
+  }
+
+  public double getRightY() {
+    return Controller.getY(Hand.kRight);
+  }
+
+  public double TimeTillStop(double timeTillStop) {
+    return timeTillStop = 1;
+    // if(Time >= time)
+    // {
+    //   return 0;
+    // }
+    // else
+    // {
+    //   return 10;
+    // }
   }
 }
