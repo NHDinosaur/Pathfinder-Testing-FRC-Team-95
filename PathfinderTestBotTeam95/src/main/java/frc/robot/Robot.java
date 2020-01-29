4b4789b4609e565c3d3e4e101aea2738129fe6f6
PJG3000\Philip <philip@sugargroup.net>
2020-01-28 20:52:05 -0500

    Set up the Pathfinder stuff and got the jaci imports resolved. It's quitting when it tries to get the encoder values and I was working on fixing that when I had to leave.

--- PathfinderTestBotTeam95/src/main/java/frc/robot/Robot.java
+++ PathfinderTestBotTeam95/src/main/java/frc/robot/Robot.java
@@ -13,17 +13,24 @@ package frc.robot;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import java.nio.file.Path;
+
 import com.ctre.phoenix.motorcontrol.ControlMode;
 import com.ctre.phoenix.motorcontrol.IMotorControllerEnhanced;
+import com.ctre.phoenix.motorcontrol.IMotorController;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.*;
+import com.ctre.phoenix.motorcontrol.*;
 //import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 
-// import jaci.pathfinder.Pathfinder;
-// import jaci.pathfinder.Trajectory;
-// import jaci.pathfinder.Waypoint;
-// import jaci.pathfinder.followers.EncoderFollower;
-// import jaci.pathfinder.modifiers.TankModifier;
+import jaci.pathfinder.Pathfinder;
+import jaci.pathfinder.Trajectory;
+import jaci.pathfinder.Waypoint;
+import jaci.pathfinder.followers.EncoderFollower;
+import jaci.pathfinder.modifiers.TankModifier;
 
 /**
  * This is a demo program showing the use of the RobotDrive class, specifically
@@ -41,25 +48,60 @@ public class Robot extends TimedRobot {
 
   private XboxController Controller = new XboxController(1);;
 
-  private IMotorControllerEnhanced Left, Right;
+  private IMotorControllerEnhanced Left, LFollower1, LFollower2, Right, RFollower1, RFollower2;
+
+  private Faults Faults = new Faults();
 
   private int leftCanNum = 10;
-  private int rightCanNum = 22;
+  private int lFollowerCanNum1 = 11;
+  private int lFollowerCanNum2 = 12;
+  private int rightCanNum = 20;
+  private int rFollowerCanNum1 = 21;
+  private int rFollowerCanNum2 = 22;
 
   private double Time = 0;
+
+  private int PID_IDX = 0;
+  private int CAN_TIMEOUT_MS = 10;
+
+
+  private double Encoder_Pos = getPositionInches();
+  private double Left_To_Right_Offset_Inches;
+  private double Top_Speed;
+  public int Right_Encoder_Pos = 0;
+  public int Left_Encoder_Pos = 0;
+  private double wheel_diameter = 5*0.0254;
+  public static double outputLeft, outputRight;
+  public static int whatPath;
+  public static int spin = 0;
 
   // private double timeAtStop = 10;
 
   @Override
   public void robotInit() {
     Left = new TalonSRX(leftCanNum); //new DifferentialDrive(new PWMVictorSPX(0), new PWMVictorSPX(1));
+    LFollower1 = new TalonSRX(lFollowerCanNum1);
+    LFollower2 = new TalonSRX(lFollowerCanNum2);
     Right = new TalonSRX(rightCanNum);
+    RFollower1 = new TalonSRX(rFollowerCanNum1);
+    RFollower2 = new TalonSRX(rFollowerCanNum2);
+
+    LFollower1.set(ControlMode.Follower, leftCanNum);
+    LFollower2.set(ControlMode.Follower, leftCanNum);
+    RFollower1.set(ControlMode.Follower, rightCanNum);
+    RFollower2.set(ControlMode.Follower, rightCanNum);
+
+    Left.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, PID_IDX, CAN_TIMEOUT_MS);
+    Left.setSensorPhase(true);
     // LeftStick = new Joystick(1);
     // RightStick = new Joystick(5);
   }
 
   @Override
   public void teleopPeriodic() {
+
+    System.out.println("Right Y value = " + getRightY() + ", left Y value = " + getLeftY());
+
     // m_myRobot.tankDrive(LeftStick.getY(), RightStick.getY());
     Left.set(ControlMode.PercentOutput, getLeftY());
     Right.set(ControlMode.PercentOutput, getRightY());
@@ -67,30 +109,40 @@ public class Robot extends TimedRobot {
 
   @Override
   public void autonomousInit() {
+    Left.setInverted(false);
 
+    PathFinderCommand(false, false, 24, 1);
   }
 
   @Override
   public void autonomousPeriodic() {
+
+    outputLeft = leftEncFollower.calculate(Left_Encoder_Pos);
+    outputRight = rightEncFollower.calculate(Right_Encoder_Pos);
+
+    Left.getFaults(Faults);
+
+    Left.set(ControlMode.PercentOutput, outputLeft);
+    Right.set(ControlMode.PercentOutput, outputRight);
     // System.out.println("We are in auto periodic");
 
     // Left.set(ControlMode.PercentOutput, 20);
     // Right.set(ControlMode.Position, 1);
-    Time++;
-    System.out.println("Time = " + Time);
+    // Time++;
+    // System.out.println("Time = " + Time);
 
-    if(Time <= 50)
-    {
-      Left.set(ControlMode.PercentOutput, 1);
-    }
-    else if(Time >= 50 && Time <= 100)
-    {
-      Left.set(ControlMode.PercentOutput, 0.5);
-    }
-    else
-    {
-      Left.set(ControlMode.Disabled, 0);
-    }
+    // if(Time <= 50)
+    // {
+    //   Left.set(ControlMode.PercentOutput, 1);
+    // }
+    // else if(Time >= 50 && Time <= 100)
+    // {
+    //   Left.set(ControlMode.PercentOutput, 0.5);
+    // }
+    // else
+    // {
+    //   Left.set(ControlMode.Disabled, 0);
+    // }
   }
 
   public double getLeftY() {
@@ -113,88 +165,84 @@ public class Robot extends TimedRobot {
     // }
   }
 
-  // private double Encoder_Pos = DrivePod.getPositionInches();
-  // private double Left_To_Right_Offset_Inches;
-  // private double Top_Speed;
-  // public int Right_Encoder_Pos = 0;
-  // public int Left_Encoder_Pos = 0;
-  // private double wheel_diameter = 5*0.0254;
-  // public static double outputLeft, outputRight;
-  // public static int whatPath;
-  // public static int spin = 0;
+  public void log() {
+    
+  }
 
-  // // All of these are the possible path values
-  // public static int ForwardTenFeet = 1;
+  public double getPositionInches() {
+    return Left.getSelectedSensorPosition(PID_IDX);
+  }
 
-  // private static boolean LeftOrRight;
-  // private static boolean WhatGearAreWeIn;
+  // All of these are the possible path values
+  public static int ForwardTenFeet = 1;
 
-  // public static EncoderFollower leftEncFollower;
-  // public static EncoderFollower rightEncFollower;
+  private static boolean LeftOrRight;
+  private static boolean WhatGearAreWeIn;
 
-  // public static Waypoint[] points;
+  public static EncoderFollower leftEncFollower;
+  public static EncoderFollower rightEncFollower;
 
-  // public PathFinderCommand(boolean RightOrLeft, boolean WhichGearAreWeIn, double a, int whatPath) {
-  //   //requires(Robot.drivebase);
-  //   //requires(Robot.pathfinder);
-  //   System.out.println("We are in PathfinderCommand");
-  //   // this.setInterruptible(false);
-  //   //System.out.println("we are in the constructor");
-  //   this.LeftOrRight = RightOrLeft;
-  //   this.WhatGearAreWeIn = WhichGearAreWeIn;
-  //   this.Left_To_Right_Offset_Inches = a;
-  //   this.whatPath = whatPath;
+  public static Waypoint[] points;
 
-    // if (whatPath == ForwardTenFeet)
-    //   {
-    //     points = new Waypoint[] {
-    //       new Waypoint(0, 0, 0),
-    //       new Waypoint(10, 0, 0)
-        // };
-  // }
+  public void PathFinderCommand(boolean RightOrLeft, boolean WhichGearAreWeIn, double a, int whatPath) {
+    //requires(Robot.drivebase);
+    //requires(Robot.pathfinder);
+    System.out.println("We are in PathfinderCommand");
+    // this.setInterruptible(false);
+    //System.out.println("we are in the constructor");
+    this.LeftOrRight = RightOrLeft;
+    this.WhatGearAreWeIn = WhichGearAreWeIn;
+    this.Left_To_Right_Offset_Inches = a;
+    this.whatPath = whatPath;
 
-   //   Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
-  //   Trajectory trajectory = Pathfinder.generate(points, config);
+    if (whatPath == ForwardTenFeet)
+      {
+        points = new Waypoint[] {
+          new Waypoint(0, 0, 0),
+          new Waypoint(10, 0, 0)
+        };
+      }
 
-  //   // Wheelbase Width = 0.5m
-  //   TankModifier modifier = new TankModifier(trajectory).modify(0.5);
+    Trajectory.Config config = new Trajectory.Config(Trajectory.FitMethod.HERMITE_CUBIC, Trajectory.Config.SAMPLES_HIGH, 0.05, 1.7, 2.0, 60.0);
+    Trajectory trajectory = Pathfinder.generate(points, config);
 
-  //   // Do something with the new Trajectories...
-  //   //Trajectory left = modifier.getLeftTrajectory();
-  //   //Trajectory right = modifier.getRightTrajectory();
+    // Wheelbase Width = 0.5m
+    TankModifier modifier = new TankModifier(trajectory).modify(0.5);
 
-  //   leftEncFollower = new EncoderFollower(modifier.getLeftTrajectory());
-  //   rightEncFollower = new EncoderFollower(modifier.getRightTrajectory());
+    // Do something with the new Trajectories...
+    //Trajectory left = modifier.getLeftTrajectory();
+    //Trajectory right = modifier.getRightTrajectory();
 
-  //   // Determine whether the encoder position is the left or right encoder position.
-  //   if(LeftOrRight)
-  //   {
-  //     Left_Encoder_Pos = (int) (Math.round(Encoder_Pos) + Left_To_Right_Offset_Inches);
-  //     Right_Encoder_Pos = (int) (Math.round(Encoder_Pos));
-  //   }
-  //   else
-  //   {
-  //     Left_Encoder_Pos = (int) (Math.round(Encoder_Pos));
-  //     Right_Encoder_Pos = (int) (Math.round(Encoder_Pos) + Left_To_Right_Offset_Inches);
-  //   }
+    leftEncFollower = new EncoderFollower(modifier.getLeftTrajectory());
+    rightEncFollower = new EncoderFollower(modifier.getRightTrajectory());
 
-  //   leftEncFollower.configureEncoder(Left_Encoder_Pos, 1000, wheel_diameter);
-  //   rightEncFollower.configureEncoder(Right_Encoder_Pos, 1000, wheel_diameter);
+    // Determine whether the encoder position is the left or right encoder position.
+    if(LeftOrRight)
+    {
+      Left_Encoder_Pos = (int) (Math.round(Encoder_Pos) + Left_To_Right_Offset_Inches);
+      Right_Encoder_Pos = (int) (Math.round(Encoder_Pos));
+    }
+    else
+    {
+      Left_Encoder_Pos = (int) (Math.round(Encoder_Pos));
+      Right_Encoder_Pos = (int) (Math.round(Encoder_Pos) + Left_To_Right_Offset_Inches);
+    }
 
-  //   // Determine what gear we are in, then make the top speed here equal to the top speed for the gear.
-  //   if(WhatGearAreWeIn)
-  //   {
-  //     Top_Speed = Constants.ROBOT_TOP_SPEED_HIGH_GEAR_FPS;
-  //   }
-  //   else
-  //   {
-  //     Top_Speed = Constants.ROBOT_TOP_SPEED_LOW_GEAR_FPS;
-  //   }
+    leftEncFollower.configureEncoder(Left_Encoder_Pos, 1000, wheel_diameter);
+    rightEncFollower.configureEncoder(Right_Encoder_Pos, 1000, wheel_diameter);
 
-  //   leftEncFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / Top_Speed, 0);
-  //   rightEncFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / Top_Speed, 0);
-  // }
+    // Determine what gear we are in, then make the top speed here equal to the top speed for the gear.
+    if(WhatGearAreWeIn)
+    {
+      Top_Speed = 1;
+    }
+    else
+    {
+      Top_Speed = 2;
+    }
 
-  // outputLeft = leftEncFollower.calculate(Left_Encoder_Pos);
-    // outputRight = rightEncFollower.calculate(Right_Encoder_Pos);
+    leftEncFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / Top_Speed, 0);
+    rightEncFollower.configurePIDVA(1.0, 0.0, 0.0, 1 / Top_Speed, 0);
+  }
+
 }
